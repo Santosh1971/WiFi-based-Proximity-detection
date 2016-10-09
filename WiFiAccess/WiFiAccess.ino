37dbe2b6ce472b68ff03f01ceaa1266ad000d114
Santosh Kumar Jha <jha.santosh.kr@gmail.com>
2016-10-09 15:23:58 +0530

    1st upload

---
+++ WiFiAccess/WiFiAccess.ino
@@ -0,0 +1,63 @@
+
+// WiFi Server of Proximity Detection
+
+//Ref:
+//https://github.com/esp8266/Arduino/issues/1664
+//http://www.esp8266.com/viewtopic.php?f=29&t=2153
+
+
+#include <ESP8266WiFi.h>
+#include<WiFiClient.h> 
+#include<ESP8266WebServer.h>`
+
+const char *ssid = "SANTOSH_AP";
+const char *password = "watchdog";
+
+ESP8266WebServer server(80);
+
+void handle_led1(){
+  digitalWrite(2, 1);
+  server.send(200, "text/plain", String("LED ON "));
+  Serial.println("LED ON "); 
+}
+
+void handle_led0(){
+  digitalWrite(2, 0);
+  server.send(200, "text/plain", String("LED OFF ")); 
+  Serial.println("LED OFF ");
+}
+
+
+void handle_myid() {
+  String state=server.arg("state");
+  String Clientid = state;
+  server.send(200, "text/plain", "myid is: " + Clientid);
+  Serial.println(Clientid);
+};
+
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  // prepare GPIO2
+  pinMode(2, OUTPUT);
+  digitalWrite(2, 0);
+
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+  Serial.println("HTTP server started");
+  server.on("/led1",handle_led1);
+  server.on("/led0",handle_led0);
+  server.on("/myid",handle_myid);
+}
+
+void loop() {
+  server.handleClient();
+}
---
+++ WiFiClient/WiFiClient.ino
@@ -0,0 +1,106 @@
+
+// WiFi Client of Proximity Detection
+
+//Ref:
+//https://github.com/esp8266/Arduino/issues/1664
+//http://www.esp8266.com/viewtopic.php?f=29&t=2153
+//http://internetofhomethings.com/homethings/?p=1270
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+
+const char* ssid     = "SANTOSH_AP";
+const char* password = "watchdog";
+const char* host = "http://192.168.4.1";
+HTTPClient http;
+  char Client_MAC[50];
+
+void rptaSrv(int httpCode){
+  if(httpCode == 200) {
+           String payload = http.getString();
+           Serial.println(payload);
+           if(payload=="LED ON "){
+            digitalWrite(2, 1);
+           }
+           else if(payload=="LED OFF "){
+            digitalWrite(2, 0);
+           }
+
+        } else {
+            Serial.print("[HTTP] GET... failed, no connection or no HTTP server\n");
+            
+            ESP.restart();
+       
+        }
+}
+
+/********************************************************
+   ADD Prefix with MAC of WiFi
+ ********************************************************/
+
+void AddMAC(char * prefix, char * topic) {
+  uint8_t MAC_array[6];
+  WiFi.macAddress(MAC_array);
+  sprintf(topic, "%s", prefix);
+  for (int i = 0; i < sizeof(MAC_array); ++i) {
+    sprintf(topic, "%s%02x", topic, MAC_array[i]);
+  }
+}
+
+/********************************************************/
+
+
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(10);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, 0);
+  Serial.println("proyecto inv client-server");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);  //sin esta no funciona
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop(){
+        if(WiFi.status() != WL_CONNECTED){
+          WiFi.begin(ssid, password);
+          delay(500);
+        }else{
+
+        Serial.print("[HTTP] begin...\n");
+        // configure traged server and url
+
+   
+          AddMAC("Client_MAC:", Client_MAC);
+          Serial.println(Client_MAC);
+          String str1=Client_MAC;
+
+
+          http.begin("192.168.4.1", 80,( "/myid?state="+ str1)); //HTTp
+        int httpCode0 = http.GET();
+        rptaSrv(httpCode0);
+        delay(500);
+          
+        http.begin("192.168.4.1", 80, "/led0"); //HTTp
+        int httpCode1 = http.GET();
+        rptaSrv(httpCode1);
+        delay(500);
+        http.begin("192.168.4.1", 80, "/led1"); //HTTP
+        int httpCode2 = http.GET();
+        rptaSrv(httpCode2);
+        delay(500);
+    }
+}
